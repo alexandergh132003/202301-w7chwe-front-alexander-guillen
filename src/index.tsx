3bc8c14651140261f28e865eee8a041da790e87e
Alexander <aguillenhernandez95@gmail.com>
2023-02-25 21:04:38 +0100

    Add form component and its test

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
+import GlobalStyles from "./GlobalStyles";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -10,6 +12,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyles />
       <App />
     </Provider>
   </React.StrictMode>
