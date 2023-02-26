4cd9c09b5e5497f02cf787ae4ec44616d2452efb
Alexander <aguillenhernandez95@gmail.com>
2023-02-26 15:46:38 +0100

    Add handleLoginClick function

--- src/components/Form/Form.test.tsx
+++ src/components/Form/Form.test.tsx
@@ -1,17 +1,22 @@
 import { render, screen } from "@testing-library/react";
+import FakeWrapper from "../../mocks/FakeWrapper";
 import Form from "./Form";
 
 describe("Given a Form component", () => {
+  render(
+    <FakeWrapper>
+      <Form />
+    </FakeWrapper>
+  );
+
+  const button = screen.getByRole("button", {
+    name: "Login",
+  });
+
   describe("When it renders itself", () => {
     test("Then it should show a title 'Social Network' and button 'Login'", () => {
-      render(<Form />);
-
       const title = screen.getByRole("heading", {
         name: "Social Network",
-      });
-
-      const button = screen.getByRole("button", {
-        name: "Login",
       });
 
       expect(title).toBeInTheDocument();
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,6 +1,21 @@
+import { useState } from "react";
+import useUser from "../../hooks/useUser/useUser";
+
 import FormStyled from "./FormStyled";
 
 const Form = (): JSX.Element => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { loginUser } = useUser();
+
+  const handleLoginClick = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    debugger;
+    loginUser({ username, password });
+    setUsername("");
+    setPassword("");
+  };
+
   return (
     <FormStyled>
       <h2 className="form-title">Social Network</h2>
@@ -13,6 +28,7 @@ const Form = (): JSX.Element => {
           id="form1Example1"
           className="form-control"
           placeholder="username"
+          onChange={(event) => setUsername(event.target.value)}
         />
       </div>
       <div className="form-outline mb-4">
@@ -24,11 +40,16 @@ const Form = (): JSX.Element => {
           id="form1Example2"
           className="form-control"
           placeholder="password"
+          onChange={(event) => setPassword(event.target.value)}
         />
       </div>
       <div className="row mb-4">
         <div className="col d-flex justify-content-center">
-          <button type="submit" className="btn btn-primary btn-block">
+          <button
+            type="submit"
+            className="btn btn-primary btn-block"
+            onClick={handleLoginClick}
+          >
             Login
           </button>
         </div>
